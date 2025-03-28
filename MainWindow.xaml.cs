18681c43e3cea0ecb6cc71d406802fe24f62edaf
FMVACHOO <vlad-efimov-2015@mail.ru>
2025-03-28 14:08:33 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using InteractiveDataDisplay.WPF;
+
+namespace FlawDetector
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using InteractiveDataDisplay.WPF;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FlawDetector
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<string> _dataFiles = new List<string>();
+
+        public MainViewModel()
+        {
+            StartVisualization();
+        }
+
+        private void StartVisualization()
+        {
+            string directoryPath = @"D:\WORK\FlawDetector\signals";
+            _dataFiles = Directory.GetFiles(directoryPath, "*.txt").ToList();
+
+            List<double> x = new List<double>(); //Array.Empty<double>();
+            List<double> y = new List<double>(); //Array.Empty<double>();
+
+            // create the lines and describe their styling
+            var graf = new InteractiveDataDisplay.WPF.LineGraph
+            {
+                Stroke = new SolidColorBrush(Colors.Blue),
+                Description = "Line A",
+                StrokeThickness = 2
+            };
+
+            foreach( var line in _dataFiles)
+            {
+                string[] values = File.ReadAllLines(line);
+                //Array.Clear(x);
+                //Array.Clear(y);
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    y[i] = Convert.ToDouble(values[i]);//ошибка выход за пределы массива
+                    x[i] = Convert.ToDouble(i) / 100.0;
+                }
+
+                // load data into the lines
+                graf.Plot(x, y);
+
+                // add lines into the grid
+                //myGrid.Children.Clear();
+                //myGrid.Children.Add(graf);
+
+                //// customize styling
+                //myChart.Title = $"Line Plot ({pointCount:n0} points each)";
+                //myChart.BottomTitle = $"Horizontal Axis Label";
+                //myChart.LeftTitle = $"Vertical Axis Label";
+                //myChart.IsAutoFitEnabled = true;
+                //myChart.LegendVisibility = Visibility.Visible;
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
